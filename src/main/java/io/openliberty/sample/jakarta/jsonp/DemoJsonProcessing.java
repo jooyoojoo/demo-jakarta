1421d3ad0b1e373044eb2f94deb2071e542ca5d4
YijiaJing <47167363+YijiaJing@users.noreply.github.com>
2022-02-01 20:54:35 -0500

    Added comments

--- src/main/java/io/openliberty/sample/jakarta/jsonp/DemoJsonProcessing.java
+++ src/main/java/io/openliberty/sample/jakarta/jsonp/DemoJsonProcessing.java
@@ -1,29 +1,28 @@
 package io.openliberty.sample.jakarta.jsonp;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.json.*;
 import javax.json.stream.JsonGenerator;
-import javax.json.stream.JsonParser;
-import javax.json.stream.JsonParser.*;
-import javax.json.stream.JsonParserFactory;
-
 
 public class DemoJsonProcessing {
 	
 	public static void main(String[] args) {
+		// process JSON object from a file
 		writeJson(readJson("src/main/java/io/openliberty/sample/jakarta/jsonp/data.json"));
+		// process a fresh new JSON object
 		writeJson(buildJson());
 	}
 	
+	/**
+	 * Reads from a JSON file
+	 */
 	public static JsonObject readJson(String filepath) {
 		try {
 			InputStream is = new FileInputStream(filepath);
@@ -39,22 +38,43 @@ public class DemoJsonProcessing {
 		return null;
 	}
 	
+	/**
+	 * Writes a JSON object and prints it
+	 */
 	public static void writeJson(JsonObject jsonObject) {
 		Map<String, Boolean> config = new HashMap<String, Boolean>();
 		config.put(JsonGenerator.PRETTY_PRINTING, true);
-		JsonWriterFactory factory = Json.createWriterFactory(config);
 		StringWriter sw = new StringWriter();
 		
-		try (JsonWriter jsonWriter = factory.createWriter(sw)) {
-			jsonWriter.writeObject(jsonObject);
-			System.out.println(sw);
-		}
+		// Use a factory to create multiple instances of writer
+		JsonWriterFactory factory = Json.createWriterFactory(config);
+		JsonWriter jsonWriter = factory.createWriter(sw);
+		jsonWriter.writeObject(jsonObject);
+		//jsonWriter.write(jsonObject);
+		jsonWriter.close();
+		System.out.println(sw);
+		
 	}
 	
+	public static void writeJsonWithoutFactory(JsonObject jsonObject) {
+		Map<String, Boolean> config = new HashMap<String, Boolean>();
+		config.put(JsonGenerator.PRETTY_PRINTING, true);
+		StringWriter sw = new StringWriter();
+		
+		// Create a writer directly
+		JsonWriter jsonWriter = Json.createWriter(sw);
+		jsonWriter.writeObject(jsonObject);
+		System.out.println(sw);
+		
+	}
+	
+	/**
+	 * Builds a new JSON object
+	 */
 	public static JsonObject buildJson() {
 		Map<String, Boolean> config = new HashMap<String, Boolean>();
-		
 		JsonBuilderFactory factory = Json.createBuilderFactory(config);
+		
 		JsonObject jsonObject = factory.createObjectBuilder()
 				.add("name", "Jane Doe")
 				.add("birthday", LocalDate.of(1995, 6, 12).toString())
